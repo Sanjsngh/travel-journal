ea95c8dd61981152c998ee4ec7ad02f3da74232a
sanjsngh <sanjivanisngh@gmail.com>
2022-06-29 15:39:18 +0530

    travel-journal React App deployed

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styleJournal.css';
+// import App from './projects/MemeGenerator/App';
+// import App from './projects/DigitalBusinessCard/App';
+import App from './projects/TravelJournal/App';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<App />);
